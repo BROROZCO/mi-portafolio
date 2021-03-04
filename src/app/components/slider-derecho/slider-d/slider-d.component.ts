59bc95f7121d0bdc274fb133cfee3379daf52704
Elinvisible <unenes.viejos@gmail.com>
2021-03-04 16:46:00 -0600

    Slider completo pero sin efectos

--- src/app/components/slider-derecho/slider-d/slider-d.component.ts
+++ src/app/components/slider-derecho/slider-d/slider-d.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderDComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
   }
+
+
+
+
 
 }
